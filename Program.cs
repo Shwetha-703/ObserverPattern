b06da106fe6856f0cf8035a87d527e9aa9a99de0
Shwetha-703 <shweta.shingare11@gmail.com>
2023-08-07 22:35:18 +0530

    Basic setup

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Observer_Pattern.Services;
+
 Console.WriteLine("Hello, World!");
 
+IHost _host = Host.CreateDefaultBuilder()
+                  .ConfigureServices(services =>
+                    {
+                        services.AddSingleton<IApplication, Application>();
+                    })
+                  .Build();
+
+var app = _host.Services.GetRequiredService<IApplication>();
+app.Run();
---
+++ Services/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Observer_Pattern.Services
+{
+
+	public interface IApplication
+	{
+		void Run();
+	}
+
+	public class Application : IApplication
+	{
+
+        public void Run()
+        {
+			Console.WriteLine("Observer pattern!");
+        }
+    }
+}
+
